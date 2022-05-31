d101298067ca67fded3c2c7eae284af2b6d87b0c
raficraft <raficraft@gmail.com>
2022-05-31 18:50:29 +0200

    first commit

---
+++ core/components/Header/Header.test.js
@@ -0,0 +1,8 @@
+import { render, prettyDOM, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
+
+test("header component and child  is render", () => {
+  const toto = 0;
+
+  expect(toto).toBe(0);
+});
