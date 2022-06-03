fffa4f7a2a6f409741d547752f1d743b2d742783
raficraft <raficraft@gmail.com>
2022-06-03 13:20:12 +0200

    test loader for app

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,30 @@
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
 
-export default class MyDocument extends Document {
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
