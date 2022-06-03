da630c86bd3df9982a4759063b3bcad2b8e3a1eb
raficraft <raficraft@gmail.com>
2022-06-03 09:57:34 +0200

    test for optimize cssinjs

--- core/components/Footer/Footer.js
+++ core/components/Footer/Footer.js
@@ -18,47 +18,49 @@ import Navigation from "../Navigation/Navigation";
 import { FooterContainer } from "./Footer_css";
 
 export default function Footer() {
-  return (
-    <FooterContainer>
-      <section className="left">
-        <header>
-          <Logo className="logo"></Logo>
-          <PhotosnapTitle color="white"></PhotosnapTitle>
-        </header>
-        <Navigation array={nav_footer}></Navigation>
-        <footer className="social_icon">
-          <div className="svgBox">
-            <IconFacebook className="svg-icon" color="white"></IconFacebook>
-            <IconFacebookColor className="svg-icon svg-icon--hover"></IconFacebookColor>
-          </div>
-          <div className="svgBox">
-            <IconYoutubeCircle
-              className="svg-icon"
-              color="white"
-            ></IconYoutubeCircle>
-            <IconYoutubeCircleColor className="svg-icon svg-icon--hover"></IconYoutubeCircleColor>
-          </div>
-          <div className="svgBox">
-            <IconTwitter className="svg-icon" color="white"></IconTwitter>
-            <IconTwitterColor className="svg-icon svg-icon--hover"></IconTwitterColor>
-          </div>
-          <div className="svgBox">
-            <IconPinterest className="svg-icon" color="white"></IconPinterest>
-            <IconPinterestColor className="svg-icon svg-icon--hover"></IconPinterestColor>
-          </div>
-          <div className="svgBox">
-            <IconInstagram className="svg-icon" color="white"></IconInstagram>
-            <IconInstagramColor className="svg-icon svg-icon--hover"></IconInstagramColor>
-          </div>
-        </footer>
-      </section>
-      <section className="middle">
-        <Navigation array={nav_footer}></Navigation>
-      </section>
-      <section className="right">
-        <Btn type="arrow_link" text="GET AN INVITE"></Btn>
-        <p className="text grey">Copyright 2019. All Rights Reserved</p>
-      </section>
-    </FooterContainer>
-  );
+  {
+    FooterContainer && (
+      <FooterContainer>
+        <section className="left">
+          <header>
+            <Logo className="logo"></Logo>
+            <PhotosnapTitle color="white"></PhotosnapTitle>
+          </header>
+          <Navigation array={nav_footer}></Navigation>
+          <footer className="social_icon">
+            <div className="svgBox">
+              <IconFacebook className="svg-icon" color="white"></IconFacebook>
+              <IconFacebookColor className="svg-icon svg-icon--hover"></IconFacebookColor>
+            </div>
+            <div className="svgBox">
+              <IconYoutubeCircle
+                className="svg-icon"
+                color="white"
+              ></IconYoutubeCircle>
+              <IconYoutubeCircleColor className="svg-icon svg-icon--hover"></IconYoutubeCircleColor>
+            </div>
+            <div className="svgBox">
+              <IconTwitter className="svg-icon" color="white"></IconTwitter>
+              <IconTwitterColor className="svg-icon svg-icon--hover"></IconTwitterColor>
+            </div>
+            <div className="svgBox">
+              <IconPinterest className="svg-icon" color="white"></IconPinterest>
+              <IconPinterestColor className="svg-icon svg-icon--hover"></IconPinterestColor>
+            </div>
+            <div className="svgBox">
+              <IconInstagram className="svg-icon" color="white"></IconInstagram>
+              <IconInstagramColor className="svg-icon svg-icon--hover"></IconInstagramColor>
+            </div>
+          </footer>
+        </section>
+        <section className="middle">
+          <Navigation array={nav_footer}></Navigation>
+        </section>
+        <section className="right">
+          <Btn type="arrow_link" text="GET AN INVITE"></Btn>
+          <p className="text grey">Copyright 2019. All Rights Reserved</p>
+        </section>
+      </FooterContainer>
+    );
+  }
 }
