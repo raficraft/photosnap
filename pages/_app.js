e4434567f14ff58ac58987f43efc0a27022fea29
raficraft <raficraft@gmail.com>
2022-06-03 13:16:34 +0200

    test loader for app

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,12 @@
 import "../styles/globals.css";
 import "../styles/designSystem.css";
 import Layout from "../core/components/Layout/Layout";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    router.events.on("routeChangeError", (e) => setLoading(false));
-    router.events.on("routeChangeStart", (e) => setLoading(false));
-    router.events.on("routeChangeComplete", (e) => setLoading(true));
-
-    return () => {
-      router.events.off("routeChangeError", (e) => setLoading(false));
-      router.events.off("routeChangeStart", (e) => setLoading(false));
-      router.events.off("routeChangeComplete", (e) => setLoading(true));
-    };
-  }, [router.events]);
   return (
-    <>
-      {loading ? (
-        <h1>Loading</h1>
-      ) : (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      )}
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
