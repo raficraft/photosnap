17b62f114e68dcc4070e8d1261885c3b0a53fcd8
raficraft <raficraft@gmail.com>
2022-06-03 10:50:43 +0200

    test loader for app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import "../styles/globals.css";
 import "../styles/designSystem.css";
 import Layout from "../core/components/Layout/Layout";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return (
+  const [appIsloaded, setAppIsloaded] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("DOMContentLoaded", () => {
+      setAppIsloaded(true);
+    });
+  }, []);
+
+  appIsloaded && (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
+
+  retrun(<h1>Loading</h1>);
 }
 
 export default MyApp;
