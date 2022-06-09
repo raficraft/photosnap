dd3f62c27f764a9bbd77b00190237793c009b6fb
raficraft <raficraft@gmail.com>
2022-06-09 09:55:19 +0200

    Fix on the home link of footer

--- core/data/navigation_data.js
+++ core/data/navigation_data.js
@@ -5,7 +5,7 @@ export const nav_header = [
 ];
 
 export const nav_footer = [
-  { link: "/home", text: "home" },
+  { link: "/", text: "home" },
   { link: "/stories", text: "stories" },
   { link: "/features", text: "features" },
   { link: "/pricing", text: "pricing" },
